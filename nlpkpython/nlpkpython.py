4993934d313d3d28f36914840fae866548a16f20
Ricardo Neves <ricardo.neves@m4u.com.br>
2017-05-18 09:50:11 -0300

    with chunking

--- nlpkpython/nlpkpython.py
+++ nlpkpython/nlpkpython.py
@@ -11,18 +11,17 @@ from nltk.stem import PorterStemmer
 from nltk.corpus import state_union
 from nltk.tokenize import PunktSentenceTokenizer
 
-#example_test = []
-#
-#with open('exemplo_teste.txt','r') as f:
-#    for line in f:
-#       example_test = line
+
 example_test2 = "One of the largest elements to any data analysis, natural language"
 example_test = "One of the largest elements to any data analysis, natural language processing included, is pre-processing. This is the methodology used to clean up and prepare your data for analysis. "
 
+#with open('exemplo_teste.txt','r') as f:
+#    for line in f:
+#       example_test = line
+
 #train_text = state_union.raw(example_test)
 #sample_text = state_union.raw("exemplo_teste.txt")
-#print(sent_tokenize(example_test))
-#print(word_tokenize(example_test))
+
 
 stop_words = set(stopwords.words("english"))
 words = word_tokenize(example_test)
@@ -39,7 +38,12 @@ def process_content():
         for i in tokenized:
             words = nltk.word_tokenize(i)
             tagged = nltk.pos_tag(words)
-            print(tagged)
+            chunkGram = r"""Chunk: {<RB.?>*<VB.?>*<NNP><NN>?}"""
+            chunkParser = nltk.RegexpParser(chunkGram)
+            chunked = chunkParser.parse(tagged)
+            
+            
+            print(chunked)
     
     except Exception as e:
         print(str(e))
