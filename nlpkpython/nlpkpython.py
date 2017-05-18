c9a68e5abd3a1894647c710a45fe53c96eed1900
Ricardo Neves <ricardo.neves@m4u.com.br>
2017-05-18 09:18:56 -0300

    POS Tagger

--- nlpkpython/nlpkpython.py
+++ nlpkpython/nlpkpython.py
@@ -8,10 +8,19 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.stem import PorterStemmer
-f = open('exemplo_teste.txt', 'r')
-#example_test = f.read()
+from nltk.corpus import state_union
+from nltk.tokenize import PunktSentenceTokenizer
+
+#example_test = []
+#
+#with open('exemplo_teste.txt','r') as f:
+#    for line in f:
+#       example_test = line
+example_test2 = "One of the largest elements to any data analysis, natural language"
 example_test = "One of the largest elements to any data analysis, natural language processing included, is pre-processing. This is the methodology used to clean up and prepare your data for analysis. "
 
+#train_text = state_union.raw(example_test)
+#sample_text = state_union.raw("exemplo_teste.txt")
 #print(sent_tokenize(example_test))
 #print(word_tokenize(example_test))
 
@@ -21,11 +30,24 @@ filtered_sentence = []
 for w in words:
     if w not in stop_words:
         filtered_sentence.append(w)
-        
- # print(filtered_sentence)
+
+custom_sent_tokenizer = PunktSentenceTokenizer(example_test2)
+tokenized = custom_sent_tokenizer.tokenize(example_test)
+
+def process_content():
+    try:
+        for i in tokenized:
+            words = nltk.word_tokenize(i)
+            tagged = nltk.pos_tag(words)
+            print(tagged)
+    
+    except Exception as e:
+        print(str(e))
+
+process_content()
 
 ps = PorterStemmer()
 for w in filtered_sentence:
-    print(ps.stem(w))
+   print(ps.stem(w))
 
-    
+        
